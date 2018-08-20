587c669452b11b8ad5c8bb43423a95372224e4c9
Samer Alsaydali <samsaydali@gmail.com>
2018-08-20 18:59:12 +0300

    Core commit

---
+++ app/controllers/Pages.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Samer
+ * Date: 8/20/2018
+ * Time: 6:18 PM
+ */
+
+class Pages
+{
+    public function __construct(){
+
+    }
+
+    public function index(){
+
+    }
+
+    public function about($id){
+        echo $id;
+    }
+}
