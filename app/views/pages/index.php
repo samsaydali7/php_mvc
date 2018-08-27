bc5c80d2ff89dbd43c3ee14af547155c14a32bff
samer alsaydali <samsaydali@gmail.com>
2018-08-27 23:43:30 +0300

    Update index.php

--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -1,4 +1,3 @@
 <?php require APPROOT . '/views/inc/header.php'; ?>
   <h1><?php echo $data['title']; ?></h1>
-  <p><?php echo $data['decription'] ?>
 <?php require APPROOT . '/views/inc/footer.php'; ?>
